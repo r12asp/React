f6eb7857dd9ae47404615e645939176fea56b981
r12asp <r12asp@hotmail.com>
2017-09-03 00:55:42 +1000

    up

--- new1/new1/src/index.js
+++ new1/new1/src/index.js
@@ -9,5 +9,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(
-<App/>, document.getElementById('root'));
+    <App/>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
